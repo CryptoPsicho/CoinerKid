4284afe44ba665ca010b1f4fba07ccbe38d81b36
CryptoPsicho <77224223+CryptoPsicho@users.noreply.github.com>
2022-11-22 13:03:24 +0000

    arrays and first funtion

--- script.js
+++ script.js
@@ -51,8 +51,55 @@ let wallet = [
 const food = [
   {
     name: 'apple',
-    health: 20
+    health: 20,
+    img: "https://w7.pngwing.com/pngs/765/668/png-transparent-charlotte-varenye-apple-fruit-apple-fruit-natural-foods-food-fruit-thumbnail.png"
   },
-  
+  {
+    name: 'banana',
+    health: 30,
+    img: "https://image.similarpng.com/very-thumbnail/2022/01/Banana-fruit-isolated-on-transparent-background-PNG.png"
+  },
+  {
+    name: 'kiwi',
+    health: 20,
+    img:"https://www.pintarmewarnai.com/png/thumb/qVuWdxvMuhPKklt-Kiwi-Fruit-PNG.png"
+  },
+  {
+    name: 'pizza',
+    health: 5,
+    img: "https://e7.pngegg.com/pngimages/280/611/png-clipart-pizza-hamburger-submarine-sandwich-restaurant-oven-pizza-food-cheese-thumbnail.png"
+  },
+  {
+    name: 'pasta',
+    health: 10,
+    img: "https://image.similarpng.com/very-thumbnail/2022/03/Spaghetti-in-dish-isolated-on-transparent-background-PNG.png"
+  },
+  {
+    name: 'fries',
+    health: -20,
+    img: "https://upload.wikimedia.org/wikipedia/commons/8/8e/French_Fries.png"
+  },
+  {
+    name: 'hamburger',
+    health: -10,
+    img: "https://image.similarpng.com/very-thumbnail/2020/04/Floating-burger-PNG.png"
+  },
+  {
+    name: 'candies',
+    health: -30,
+    img: "https://purepng.com/public/uploads/large/purepng.com-button-candyfood-sweet-tasty-taste-candy-941524599400vexkj.png"
+  },
 ]
 
+const locations = [
+  {
+    name: "Store",
+    "button text": ["Buy Fruit, Buy Pasta, Go Home"],
+		"button functions": [buyFruit, buyPasta, goHome],
+		text: "You enter the store. Do you wat to buy some healthy fruit or some pasta?"  
+  }
+]
+
+function buyFruit () {
+  //present fruit
+}
