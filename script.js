23625b22665f4ed39e5f18063dadd23256c23907
CryptoPsicho <77224223+CryptoPsicho@users.noreply.github.com>
2022-11-22 13:11:22 +0000

    funct

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ const text = document.querySelector("#text");
 const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
+const grid = document.querySelector('.grid');
 
 
 let heath = 100;
@@ -46,60 +47,88 @@ let wallet = [
     coin: EURO,
     quantity: 1
   }
-]
+];
 
 const food = [
   {
     name: 'apple',
     health: 20,
-    img: "https://w7.pngwing.com/pngs/765/668/png-transparent-charlotte-varenye-apple-fruit-apple-fruit-natural-foods-food-fruit-thumbnail.png"
+    img: "https://w7.pngwing.com/pngs/765/668/png-transparent-charlotte-varenye-apple-fruit-apple-fruit-natural-foods-food-fruit-thumbnail.png",
+    location: "Store",
+    type: "Fruit"
   },
   {
     name: 'banana',
     health: 30,
-    img: "https://image.similarpng.com/very-thumbnail/2022/01/Banana-fruit-isolated-on-transparent-background-PNG.png"
+    img: "https://image.similarpng.com/very-thumbnail/2022/01/Banana-fruit-isolated-on-transparent-background-PNG.png",
+    location: "Store",
+    type: "Fruit"
   },
   {
     name: 'kiwi',
     health: 20,
-    img:"https://www.pintarmewarnai.com/png/thumb/qVuWdxvMuhPKklt-Kiwi-Fruit-PNG.png"
+    img:"https://www.pintarmewarnai.com/png/thumb/qVuWdxvMuhPKklt-Kiwi-Fruit-PNG.png",
+    location: "Store",
+    type: "Fruit"
   },
   {
     name: 'pizza',
     health: 5,
-    img: "https://e7.pngegg.com/pngimages/280/611/png-clipart-pizza-hamburger-submarine-sandwich-restaurant-oven-pizza-food-cheese-thumbnail.png"
+    img: "https://e7.pngegg.com/pngimages/280/611/png-clipart-pizza-hamburger-submarine-sandwich-restaurant-oven-pizza-food-cheese-thumbnail.png",
+    location: "Restaurant",
+    type: "Food"
   },
   {
     name: 'pasta',
     health: 10,
-    img: "https://image.similarpng.com/very-thumbnail/2022/03/Spaghetti-in-dish-isolated-on-transparent-background-PNG.png"
+    img: "https://image.similarpng.com/very-thumbnail/2022/03/Spaghetti-in-dish-isolated-on-transparent-background-PNG.png",
+    location: "Restaurant",
+    type: "Food"
   },
   {
     name: 'fries',
     health: -20,
-    img: "https://upload.wikimedia.org/wikipedia/commons/8/8e/French_Fries.png"
+    img: "https://upload.wikimedia.org/wikipedia/commons/8/8e/French_Fries.png",
+    location: "McDonald",
+    type: "JunkFood"
+
   },
   {
     name: 'hamburger',
     health: -10,
-    img: "https://image.similarpng.com/very-thumbnail/2020/04/Floating-burger-PNG.png"
+    img: "https://image.similarpng.com/very-thumbnail/2020/04/Floating-burger-PNG.png",
+    location: "McDonald",
+    type: "JunkFood"
   },
   {
     name: 'candies',
     health: -30,
-    img: "https://purepng.com/public/uploads/large/purepng.com-button-candyfood-sweet-tasty-taste-candy-941524599400vexkj.png"
+    img: "https://purepng.com/public/uploads/large/purepng.com-button-candyfood-sweet-tasty-taste-candy-941524599400vexkj.png",
+    location: "Store",
+    type: "Candies"
   },
-]
+];
 
 const locations = [
   {
     name: "Store",
-    "button text": ["Buy Fruit, Buy Pasta, Go Home"],
-		"button functions": [buyFruit, buyPasta, goHome],
-		text: "You enter the store. Do you wat to buy some healthy fruit or some pasta?"  
+    "button text": ["Buy Fruit", "Buy Candies", "Go Home"],
+		"button functions": [buyFruit, buyCandies, goHome],
+		text: "You enter the store. Do you wat to buy some healthy fruit or some candies?"  
   }
-]
+];
+
+function createBoard(food) {
+  for each  {
+    const card = document.createElement('img')
+    card.setAttribute('src', 'images/blank.png')
+    card.setAttribute('data-id', i)
+    card.addEventListener('click', flipCard)
+    grid.appendChild(card)
+  }
+}
+
 
 function buyFruit () {
   //present fruit
-}
+};
