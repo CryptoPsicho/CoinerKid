c3368f8d86b4adb84eb02fb63bae406b89ee4992
CryptoPsicho <77224223+CryptoPsicho@users.noreply.github.com>
2022-11-24 16:00:56 +0000

    final??

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
-const ONECENT = 1;
-const TWOCENT = 2;
-const FIVECENT = 5;
-const TENCENT = 10;
-const TWENTYCENT = 20;
-const FIFTYCENT = 50;
-const EURO = 100;
+let price = 0;
+let payment = 0;
+let change = 0;
 
-let myfood =[]
 const foodText = document.querySelector("#foodText");
 const healthText = document.querySelector("#healthText");
 const walletText = document.querySelector("#walletText");
@@ -14,38 +9,53 @@ const text = document.querySelector("#text");
 const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
-const grid = document.querySelector('.grid');
+const foodGrid = document.querySelector('#foodGrid');
+const moneyGrid = document.querySelector('#moneyGrid');
+const paymentGrid = document.querySelector('#paymentGrid');
 
+const wallet = [
+  {
+    coin: "ONECENT",
+    value: 1,
+    quantity: 100,
+    img: "https://www.kindpng.com/picc/m/553-5532338_1-euro-cent-common-1-cent-coin-png.png"
+  },
+  {
+    coin: "TWOCENT",
+    value: 2,
+    quantity: 50,
+    img: "https://www.kindpng.com/picc/m/209-2098026_transparent-50-cent-png-5-euro-cent-clipart.png"
+  },
+  {
+    coin: "FIVECENT",
+    value: 5,
+    quantity: 20,
+    img: "https://img.favpng.com/16/24/4/euro-coins-5-cent-euro-coin-1-cent-euro-coin-10-cent-euro-coin-png-favpng-CKQdX3BssViERnLsgT9dqzADd.jpg"
 
-let heath = 100;
-let wallet = [
-  {
-    coin: ONECENT,
-    quantity: 100
   },
   {
-    coin: TWOCENT,
-    quantity: 50
+    coin: "TENCENT",
+    value: 10,
+    quantity: 10,
+    img: "https://img.favpng.com/6/20/23/euro-coins-10-cent-euro-coin-1-cent-euro-coin-50-cent-euro-coin-png-favpng-1G2Kgyp2SXe7JuKJUzHWzqtdL.jpg"
   },
   {
-    coin: FIVECENT,
-    quantity: 20
+    coin: "TWENTYCENT",
+    value: 20,
+    quantity: 5,
+    img: "https://img.favpng.com/4/12/1/20-cent-euro-coin-euro-coins-png-favpng-veN86KmhvZWMppyYwJEAWDZ0k.jpg"
   },
   {
-    coin: TENCENT,
-    quantity: 10
+    coin: "FIFTYCENT",
+    value: 50,
+    quantity: 2,
+    img: "https://img.favpng.com/15/1/23/20-cent-euro-coin-1-cent-euro-coin-20-euro-note-10-cent-euro-coin-png-favpng-MuecrPVH2spFfM1SSTwkNCCB8.jpg"
   },
   {
-    coin: TWENTYCENT,
-    quantity: 5
-  },
-  {
-    coin: FIFTYCENT,
-    quantity: 2
-  },
-  {
-    coin: EURO,
-    quantity: 1
+    coin: "EURO",
+    value: 100,
+    quantity: 1,
+    img: "https://img.favpng.com/21/7/4/coin-circle-font-png-favpng-AXXMSVqDfCFgvLpgPqu2wzFAB.jpg"
   }
 ];
 
@@ -120,13 +130,28 @@ const locations = [
     "button functions": [buyBurger, buyFries, goHome],
 		text: "You enter McDonald. Do you want to buy a burger or fries?"  
   },
-
+  {
+    name: "Change",
+    "button text": ["", "Take Change", ""],
+    "button functions": [nothing, takeChange, nothing],
+		text: ""  
+  },
 ];
 
 // initialize buttons
 button1.onclick = goStore;
 button2.onclick = goRestaurant;
 button3.onclick = goMcDonald;
+
+function createWallet() {
+  wallet.forEach(element => {
+    const coin = document.createElement('img')
+    coin.setAttribute('src', element.img)
+    coin.setAttribute('data-id', element.coin)
+    coin.addEventListener('click', addMoney)
+    moneyGrid.appendChild(coin)
+  })
+};
 
 function createBoard(location) {
   food.forEach(element => {
@@ -135,7 +160,7 @@ function createBoard(location) {
         buyFood.setAttribute('src', element.img)
         buyFood.setAttribute('data-id', element.name)
         buyFood.addEventListener('click', askMoney)
-        grid.appendChild(buyFood)
+        foodGrid.appendChild(buyFood)
     }
   });  
 };
@@ -152,27 +177,96 @@ function update(location) {
 };
 
 function goHome() {
-  update(locations[0]);
+  update(locations[0])
 };
 
 function goStore() {
-  update(locations[1]);
+  update(locations[1])
 };
 
 function goRestaurant() {
-  update(locations[2]);
+  update(locations[2])
 };
 
 function goMcDonald() {
-  update(locations[3]);
+  update(locations[3])
 };
+
+function goChange() {
+  update(location[4])
+  createWallet()
+  paymentGrid.querySelectorAll('*').forEach( n => n.remove())
+};
+
+
 
 //write askMoney function
 function askMoney () {
-
+  price = Math.floor(Math.random() * 100)
+  button1.innerText = "";
+  button2.innerText = "PAY";
+  button3.innerText = "";
+  button1.onclick = nothing();
+  button2.onclick = buy();
+  button3.onclick = nothing();
+  text.innerText = 'You have to pay ${price} cents'
+  createWallet()
 };
-function buy () {
-  //present fruit
+
+function addMoney () {
+  let coinId = this.getAttribute('data-id')
+  payment += wallet[coinId].value
+  const payedcoin = document.createElement('img')
+  coin.setAttribute('src', wallet[coinId].img)
+  coin.setAttribute('data-id', wallet[coinId].coin)
+  coin.addEventListener('click', removeCoin(coinId))
+  paymentGrid.appendChild(coin)
+
+  };
+
+function removeCoin(coin) {
+  payment -= wallet[coin].value
+  paymentGrid.removeChild(coin)
 };
 
+function takeChange() {
+  if (change == payment) {
+    alert("Correct!!")
+    goHome()
+    price = 0;
+    payment = 0;
+    change = 0;  
+  }
+  else if (change < payment) {
+    alert("You need to take more change back!")
+  } 
+  else {
+    alert("You're trying to fool me")
+  }
+}
 
+function buy() {
+  change = payment - price
+  if (change < 0) {
+    alert("You have not payed enough")
+  }
+  else if (change = 0) {
+    alert("Thank you very much! Enjoy your food and have a nice day!")
+    payment = 0
+    price = 0
+    goHome()
+  }
+  else {
+    alert("You gave me too much many. How much change do you want?")
+    goChange()
+    let payment1 = payment
+    let price1 = price
+    payment = 0
+    price = 0
+    text.innerText = "How much is the change of " + payment1 + " - " + price1 + " = "
+  }
+};
+
+function nothing() {
+  console.log("fantara verdulli!")
+};
